1053532e6f567cd419a7dc9754ce65be37906a45
navidheydari <navid.heidary@gmail.com>
2026-05-19 22:41:24 -0700

    fix: add settings property and saveSettings to main plugin

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { Plugin } from 'obsidian';
+import { DEFAULT_SETTINGS, PluginSettings, SpeechToTextSettingTab } from './settings';
 
 export default class SpeechToTextPlugin extends Plugin {
-  async onload() {
-    console.log('Speech2Text loaded');
+  settings: PluginSettings;
+
+  async onload(): Promise<void> {
+    await this.loadSettings();
+    this.addSettingTab(new SpeechToTextSettingTab(this.app, this));
+  }
+
+  async loadSettings(): Promise<void> {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings(): Promise<void> {
+    await this.saveData(this.settings);
   }
 }
